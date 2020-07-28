8c5e2b9d3566b2b302f704af16839d549f11de18
Michelle Stermitz <stermitzm@gmail.com>
2020-07-28 13:52:17 -0700

    Made mushBasket function

--- common/utils.js
+++ common/utils.js
@@ -1,6 +1,15 @@
+import { BASKET } from './constants.js'
+
 export function newUser(formData) {
     return {
         name: formData.get('name'),
         password: formData.get('password')
     };
+}
+
+export function mushBasket() {
+    const stringyBasket = localStorage.getItem(BASKET);
+    let basket = JSON.parse(stringyBasket) || [];
+    
+    return basket;
 }
