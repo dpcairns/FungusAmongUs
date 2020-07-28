b137dd15f507dec3e27ba7de802bb84f274e9a36
SarahRector <rector.sarah.r@gmail.com>
2020-07-28 13:42:58 -0700

    some data updates

--- data/mushData.js
+++ data/mushData.js
@@ -1,12 +1,12 @@
 const mushData = [
     {
-    id: mush1,
-    commonName: 'Golden Chanterelle',
-    scientificName: 'Cantharellus cibarius',
-    image: www.placekitten.com/300/300,
-    description: 'Cap bright orange to yellow-orange, bald, usually concave or wavy when mature. Gills well spaced, shallow, blunt-edged and fairly thick, often with connecting veins in between. Gills same color as cap or paler, running down the stalk. Stalk colored like cap or slightly paler, solid (not hollow). Flesh white or with a tinge of yellow. Growing on the ground.',
-    edibility: 'Very popular because of it’s cheerful color, delicate flavor, and fruity fragrance. It is usually free of maggots, which is nice!',
-    region: 'Coast, Northwestern Oregon, Southwestern Oregon'
+        id: mush1,
+        commonName: 'Golden Chanterelle',
+        scientificName: 'Cantharellus cibarius',
+        image: www.placekitten.com/300/300,
+        description: 'Cap bright orange to yellow-orange, bald, usually concave or wavy when mature. Gills well spaced, shallow, blunt-edged and fairly thick, often with connecting veins in between. Gills same color as cap or paler, running down the stalk. Stalk colored like cap or slightly paler, solid (not hollow). Flesh white or with a tinge of yellow. Growing on the ground.',
+        edibility: 'Very popular because of it’s cheerful color, delicate flavor, and fruity fragrance. It is usually free of maggots, which is nice!',
+        region: 'Coast, Northwestern Oregon, Southwestern Oregon'
     },
 
     {
