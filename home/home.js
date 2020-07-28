a8d0f2bbd5afd6928266129b9963f5d063eba844
Frank Josh Olloqui <josh.olloqui@gmail.com>
2020-07-28 10:37:16 -0700

    adds function that grabs the radio button on homepage

--- home/home.js
+++ home/home.js
@@ -1,1 +1,12 @@
-console.log('This is the homepage!');
+function setPage() {
+    const regionLabels = document.querySelectorAll('label');
+    let regionInputs = [];
+
+    for (let i = 0; i <= 4; i++) {
+        regionInputs[i] = regionLabels[i].children[0];
+    }
+
+    console.log(regionInputs);
+}
+
+setPage();
