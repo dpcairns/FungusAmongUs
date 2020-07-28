46f7d8b06185418c087f6a64cf31967aece48034
SarahRector <rector.sarah.r@gmail.com>
2020-07-28 09:46:08 -0700

    initial commit

---
+++ data/mushData.js
@@ -0,0 +1,49 @@
+const mushData = [
+    {
+    id: mush1,
+    commonName: 'Golden Chanterelle',
+    scientificName: 'Cantharellus cibarius',
+    image: www.placekitten.com/300/300,
+    description: 'Cap bright orange to yellow-orange, bald, usually concave or wavy when mature. Gills well spaced, shallow, blunt-edged and fairly thick, often with connecting veins in between. Gills same color as cap or paler, running down the stalk. Stalk colored like cap or slightly paler, solid (not hollow). Flesh white or with a tinge of yellow. Growing on the ground.',
+    edibility: 'Very popular because of it’s cheerful color, delicate flavor, and fruity fragrance. It is usually free of maggots, which is nice!',
+    region: 'Coast, Northwestern Oregon, Southwestern Oregon'
+    },
+    
+    -Oyster Mushroom (Pleurotus ostreatus)
+    
+    -Prince (Agaricus augustus)
+    
+    -King Bolete, aka Porcini (Boletus edulis)
+    
+    -Chicken of the Woods (Laetiporus sulphureus)
+    
+    -Lion’s Mane (Hericium erinaceus)
+
+-Hedgehog Mushroom (Hydnum repandum)
+
+-Cauliflower Mushroom (Sparassis crispa)
+
+-Morel (Morchella esculenta)
+
+-Lobster Mushroom (Hypomyces lactifluorum)
+
+-White Matsutake (Tricholoma magnivelare)
+
+-Fairy Ring Mushroom (Marasmius oreades)
+
+-White Chanterelle (Cantharellus subalbidus)
+
+-Honey Mushrooms (Armillaria mellea)
+
+-Shrimp Mushroom (Russula xerampelina)
+
+-Oregon White Truffle (Tuber gibbosum)
+
+-Witch’s Butter (Tremella mesenterica)
+
+-Western Giant Puffball (Calvatia booniana)
+
+-Toothed Jelly Fungus (Pseudohydnum gelatinosum)
+
+-Turkey Tail (Trametes versicolor)
+]
